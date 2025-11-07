1f666a3eadccf931307bfa9e1aaefb87e63466dc
Sehaj Chawla <sehaj@rtesoftwares.com>
2025-11-07 16:15:50 +0530

    changes

--- components/ComingSoonPage.tsx
+++ components/ComingSoonPage.tsx
@@ -40,6 +40,8 @@ const ComingSoonPage: React.FC<ComingSoonPageProps> = ({
     }
   }
 
+
+  
   return (
     <div className="flex items-center justify-center min-h-[calc(100vh-200px)]">
       <Card className="w-full max-w-md text-center">
